43bbe707926bf5e9b27c20e0353372e776a684c8
sshresthafdic <119687289+sshresthafdic@users.noreply.github.com>
2022-12-02 16:30:15 +0000

    bad code

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -20,7 +20,36 @@ public static class Program
         WriteLine($"Platform: .NET Core 2.0");
         WriteLine($"OS: {RuntimeInformation.OSDescription}");
         WriteLine();
+
+        string[] strings = { "hello", "world" };
+        string[] moreStrings = { "hello", "world" };
+        WriteLine(strings.SequenceEqual(moreStrings));
+        WriteLine(DeepEquals(strings, moreStrings));
   }
+
+  static bool DeepEquals<T>(T[] arr1, T[] arr2)
+    {
+        // If arr1 and arr2 refer to the same array, they are trivially equal.
+        if (ReferenceEquals(arr1, arr2)) return true;
+
+        // If either arr1 or arr2 is null and they are not both null (see the previous
+        // check), they are not equal.
+        if (arr1 == null || arr2 == null) return false;
+
+        // If both arrays are non-null but have different lengths, they are not equal.
+        if (arr1.Length != arr2.Length) return false;
+
+        // Failing which, do an element-by-element compare.
+        for (int i = 0; i < arr1.Length; ++i)
+        {
+            // Early out if we find corresponding array elements that are not equal.
+            if (!arr1[i].Equals(arr2[i])) return false;
+        }
+
+        // If we get here, all of the corresponding array elements were equal, so the
+        // arrays are equal.
+        return true;
+    }
 
   public static string GetBot(string message) 
   {
